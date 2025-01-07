097e558b70284eca90ab43a5722ba2e7323f97b6
mRiyaan <mohammedriyaan115@gmail.com>
2025-01-07 17:35:44 +0530

    module 1

--- module_1/Jfsd_cse_general.java
+++ module_1/Jfsd_cse_general.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
 public class Jfsd_cse_general {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+
+    }
 }
