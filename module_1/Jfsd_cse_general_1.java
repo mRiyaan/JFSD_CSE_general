b113eb694fd53c84dc0ad5fbccbfd3b26ecd99ca
mRiyaan <mohammedriyaan115@gmail.com>
2025-01-07 22:15:30 +0530

    module 1

--- module_1/Jfsd_cse_general_1.java
+++ module_1/Jfsd_cse_general_1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Jfsd_cse_general {
+public class Jfsd_cse_general_1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
