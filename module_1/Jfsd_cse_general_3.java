a06da4ae43ffff7243c5b394749ad952fc12d06d
mRiyaan <mohammedriyaan115@gmail.com>
2025-01-09 16:10:02 +0530

    module 1 exp 3 swaping

---
+++ module_1/Jfsd_cse_general_3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Jfsd_cse_general_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number to be reversed");
+        int no = sc.nextInt();
+        System.out.println("enter no of digits in the number");
+        int s = sc.nextInt();
+
+
+    }
+}
