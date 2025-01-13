130b2b40448e6061a2b7ab4c1ddd28a3fd9f85c5
mRiyaan <mohammedriyaan115@gmail.com>
2025-01-13 15:40:35 +0530

    module 1 exp 4 sorting

---
+++ module_1/Jfsd_cse_general_collections.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+public class Jfsd_cse_general_collections {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        for(int i= 0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr,1,4);
+        for(int i=0;i<n;i++){
+            System.out.println(" "+arr[i]+" ");
+        }
+    }
+}
